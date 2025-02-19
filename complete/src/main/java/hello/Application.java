d31193d1f7609e4bc8d35e6f8e7d43212e9ba69a
smitchell <stevecmitchell@gmail.com>
2025-02-19 11:01:27 -0600

    Check point commit of org.springframework.vault.VaultException: Status 403 Forbidden [secret]: 1 error occurred: permission denied

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.vault.authentication.ClientAuthentication;
+import org.springframework.vault.authentication.VaultLoginException;
 import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.core.VaultKeyValueOperationsSupport.KeyValueBackend;
 import org.springframework.vault.core.VaultSysOperations;
@@ -14,10 +15,10 @@ import org.springframework.vault.support.VaultMount;
 import org.springframework.vault.support.VaultResponse;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application implements CommandLineRunner{
 
-	// 	@Autowired
-	// private VaultTemplate vaultTemplate;
+	@Autowired
+	private VaultTemplate vaultTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -25,53 +26,54 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
+		try {
+			// You usually would not print a secret to stdout
+			VaultResponse response = vaultTemplate
+					.opsForKeyValue("secret", KeyValueBackend.KV_2).get("github");
+			if (response != null && response.getData() != null) {
+				System.out.println("Value of github.oauth2.key");
+				System.out.println("-------------------------------");
+				System.out.println(response.getData().get("github.oauth2.key"));
+				System.out.println("-------------------------------");
+			} else {
+				System.out.println("No data found for key 'github'");
+			}
 
-		// See https://stackoverflow.com/questions/67096521/error-with-spring-approleauthentication-uri-is-not-absolute
-		AppRoleAuthenticationService appRoleAuth = new AppRoleAuthenticationService("6b7b3d81-d367-32e1-7b5d-aaad7160e5ea", "7115c554-eec4-8d2d-75be-43ad7124f290", "127.0.0.0", "http", "8200");
-		VaultEndpoint vaultEp = appRoleAuth.vaultEndpoint();
-		ClientAuthentication auth = appRoleAuth.clientAuthentication();
+			// Let's encrypt some data using the Transit backend.
+			VaultTransitOperations transitOperations = vaultTemplate.opsForTransit();
 
-		VaultTemplate vaultTemplate = new VaultTemplate(vaultEp, auth);
+			// We need to setup transit first (assuming you didn't set up it yet).
+			VaultSysOperations sysOperations = vaultTemplate.opsForSys();
 
-		// You usually would not print a secret to stdout
-		VaultResponse response = vaultTemplate
-				.opsForKeyValue("secret", KeyValueBackend.KV_2).get("github");
-		System.out.println("Value of github.oauth2.key");
-		System.out.println("-------------------------------");
-		System.out.println(response.getData().get("github.oauth2.key"));
-		System.out.println("-------------------------------");
-		System.out.println();
+			if (!sysOperations.getMounts().containsKey("transit/")) {
 
-		// Let's encrypt some data using the Transit backend.
-		VaultTransitOperations transitOperations = vaultTemplate.opsForTransit();
+				sysOperations.mount("transit", VaultMount.create("transit"));
 
-		// We need to setup transit first (assuming you didn't set up it yet).
-		VaultSysOperations sysOperations = vaultTemplate.opsForSys();
+				transitOperations.createKey("foo-key");
+			}
 
-		if (!sysOperations.getMounts().containsKey("transit/")) {
+			// Encrypt a plain-text value
+			String ciphertext = transitOperations.encrypt("foo-key", "Secure message");
 
-			sysOperations.mount("transit", VaultMount.create("transit"));
+			System.out.println("Encrypted value");
+			System.out.println("-------------------------------");
+			System.out.println(ciphertext);
+			System.out.println("-------------------------------");
+			System.out.println();
 
-			transitOperations.createKey("foo-key");
+			// Decrypt
+
+			String plaintext = transitOperations.decrypt("foo-key", ciphertext);
+
+			System.out.println("Decrypted value");
+			System.out.println("-------------------------------");
+			System.out.println(plaintext);
+			System.out.println("-------------------------------");
+			System.out.println();
+		} catch (VaultLoginException e) {
+			System.out.println("Vault login failed. Check your configuration: " + e.getMessage());
+			throw new RuntimeException(e);
 		}
-
-		// Encrypt a plain-text value
-		String ciphertext = transitOperations.encrypt("foo-key", "Secure message");
-
-		System.out.println("Encrypted value");
-		System.out.println("-------------------------------");
-		System.out.println(ciphertext);
-		System.out.println("-------------------------------");
-		System.out.println();
-
-		// Decrypt
-
-		String plaintext = transitOperations.decrypt("foo-key", ciphertext);
-
-		System.out.println("Decrypted value");
-		System.out.println("-------------------------------");
-		System.out.println(plaintext);
-		System.out.println("-------------------------------");
-		System.out.println();
 	}
+
 }
